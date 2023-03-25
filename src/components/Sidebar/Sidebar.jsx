b0965e5004ae8f50b6821eb7cc4151f6bf323895
tuit212 <asadbekanvarov9808@example.com>
2023-03-25 10:50:00 +0500

    chats

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,11 +1,15 @@
 import React from 'react' 
 import Navbar from '../Navbar/Navbar'
+import Search from '../Search/Search'
+import Chats from '../Chats/Chats'
 import './Sidebar.scss'
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
       <Navbar/>
+      <Search/>
+      <Chats/>
     </div>
   )
 }
